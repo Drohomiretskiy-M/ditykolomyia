1ed329c530df954ce7af18f5970e749908768005
Misha Drohomiretskiy <drohomiretskiy@gmail.com>
2017-10-12 20:49:04 +0300

    Зверстано деталку

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -1,12 +1,23 @@
-$("#toggle").click(function() {
-  $(this).toggleClass("on");
-  $("#menu").slideToggle();
+$("#toggle").click(function () {
+    $(this).toggleClass("on");
+    $("#menu").slideToggle();
 });
-$(window).resize(function() {
-  if ($(window).width() >= '769'){
-    $("nav #menu").show();
-   };
-   if ($(window).width() <= '768' && !$("#toggle").hasClass("on")){
-    $("nav #menu").hide();
-   };
- });
+$(window).resize(function () {
+    if ($(window).width() >= '769') {
+        $("nav #menu").show();
+    };
+    if ($(window).width() <= '768' && !$("#toggle").hasClass("on")) {
+        $("nav #menu").hide();
+    };
+});
+
+$(document).ready(function () {
+    $("a.gal").fancybox({
+        'transitionIn': 'none',
+        'transitionOut': 'none',
+        'titlePosition': 'over',
+        'titleFormat': function (title, currentArray, currentIndex, currentOpts) {
+            return '<span id="fancybox-title-over">Фото ' + (currentIndex + 1) + ' / ' + currentArray.length + (title.length ? ' &nbsp; ' + title : '') + '</span>';
+        }
+    });
+});
